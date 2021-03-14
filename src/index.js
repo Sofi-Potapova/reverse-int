318c7f6f26ac36b755a71c8e9b97f5dbc31144fe
Potapova Sofia <ptv-sofia@yandex.by>
2021-03-14 22:33:40 +0300

    init: index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 module.exports = function reverse (n) {
 if(n>=0){
-    let new_string = n.toFixed(3);
+    let new_string = String(n);
     new_string=new_string.split("").reverse().join("");
-    new_string = new_string.parseFloat()
+    new_string = Number(new_string);
   return new_string;
 }
 else{
-    let new_string = n.toFixed(3);
+  let new_string=(-1)*n;
+    new_string = String(n);
     new_string=new_string.split("").reverse().join("");
-    new_string = new_string.parseFloat()
-  return -new_string;
+    new_string = Number(new_string);
+  return (-1)*new_string;
 }
 }
